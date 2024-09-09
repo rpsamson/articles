08e7a89bb66adfd09bd6a1693c80487f06930f1f
Samson Reddimalla <samson.rp@gmail.com>
2024-09-09 12:24:43 +0530

    added test email function

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,20 @@ class Home extends BaseController
 {
     public function index(): string
     {
+      //  $this->SendTestEmail();
         return view('welcome_message');
+    }
+
+    private function SendTestEmail() {
+        $email =  \Config\Services::email();
+        $email->setTo('samson.rp@gmail.com');
+        $email->setSubject('Mail from SKE AUTION - You have completed the bid');
+        $email->setMessage('<b>Great work done for SKE</b>');
+        if($email->send()) 
+        {
+            echo "Email Sent";
+        } else {
+            echo "Email Could not be send";
+        };
     }
 }
